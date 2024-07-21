6182a9148cb50ed86030ab7e6b0f51d28433e5df
ShashankNathe <shashanknathe@gmail.com>
2024-07-21 15:34:17 +0530

    server changes

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import purchasesRoutes from "./routes/purchaseRoutes.js";
 import supplierRoutes from "./routes/supplierRoutes.js";
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(cookieParser());
@@ -34,5 +34,5 @@ app.get("/", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log(`Server is running on http://localhost:${port}`);
 });
